74139c528cd71769372752bdb3a053c7865850ad
nova <nova@novas-MacBook-Air.local>
2026-01-11 11:16:18 +0100

    last commit

--- CPP01/ex06/Harl.hpp
+++ CPP01/ex06/Harl.hpp
@@ -1,18 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: szaoual <szaoual@1337.ma>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/11 07:08:57 by szaoual           #+#    #+#             */
-/*   Updated: 2026/01/11 11:01:39 by szaoual          ###   ########.fr       */
+/*   Created: 2026/01/11 11:11:00 by szaoual           #+#    #+#             */
+/*   Updated: 2026/01/11 11:13:01 by szaoual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#ifndef HARL_HPP
+# define HARL_HPP
 
-void randomChump( std::string name){
-        Zombie z(name);
-        z.announce();
+#include <iostream>
+
+class Harl
+{
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		Harl();
+		~Harl();
+		void complain(std::string level) ;
 };
+#endif
--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -5,24 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: szaoual <szaoual@1337.ma>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/11 09:17:21 by szaoual           #+#    #+#             */
-/*   Updated: 2026/01/11 11:05:53 by szaoual          ###   ########.fr       */
+/*   Created: 2026/01/11 11:09:40 by szaoual           #+#    #+#             */
+/*   Updated: 2026/01/11 11:10:37 by szaoual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Harl.hpp"
 
-int main()
+int main (int argc, char **argv)
 {
-    std::string str = "HI THIS IS BRAIN";
-    std::string *stringPTR = &str;
-    std::string &stringREF = str;
-
-    std::cout << "str address: " << &str << std::endl;
-    std::cout << "stringPTR address: " << stringPTR << std::endl;
-    std::cout << "stringREF address: " << &stringREF << std::endl;
-
-    std::cout << "str value: " << str << std::endl;
-    std::cout << "stringPTR value: " << *stringPTR << std::endl;
-    std::cout << "stringREF value: " << stringREF << std::endl;
+	std::string level;
+	Harl harl;
+	
+	if (argc != 2)
+	{
+		std::cout << "Wrong number of arguments!\n";
+		return (0);
+	}
+	level = argv[1];
+	harl.complain(level);
+	return (0);
 }
